69f8269704aaccd8294edbb33e819687dfc227bf
ivk <ivk@tutao.de>
2025-08-28 15:50:28 +0200

    Resolve path to target dir relative to js file

--- run.js
+++ run.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 
 import {spawnSync} from "node:child_process"
+import path, {dirname} from "node:path";
+import {fileURLToPath} from "node:url";
+
+const fileDir = dirname(fileURLToPath(import.meta.url))
 
 const [, , ...args] = process.argv;
+
 // FIXME windows
-const result = spawnSync("target/release/wasm-pack", args, {cwd: process.cwd(), stdio: "inherit"})
+const result = spawnSync(path.join(fileDir, "target/release/wasm-pack"), args, {cwd: process.cwd(), stdio: "inherit"})
 
 if (result.error) {
-	console.error(msg);
+	console.error(result.error);
 	process.exit(1);
 }
 
